3622452a48bfa6915e3ca481f598b2806be3f14c
nfcarbone <nfcarbone@gmail.com>
2021-02-12 10:06:49 -0500

    simplest grid implementation

--- src/Sections/Grid.js
+++ src/Sections/Grid.js
@@ -1,120 +1,46 @@
-import React, { useState, useContext, useEffect, useMemo } from 'react';
-import { Editor } from '../Providers/Editor';
-import { Instrument } from '../Providers/Instrument';
+import React from 'react';
 import { CellIcon, CircleIcon } from '../icons';
 
 export const Grid = () => {
-  const { pattern } = useContext(Editor);
-  const { instrument } = useContext(Instrument);
-  const [size, setSize] = useState(64);
-  const [sampleSize, setSampleSize] = useState(9);
-
-  useEffect(() => {
-    if (pattern.length !== size) setSize(pattern.length);
-  }, [pattern.length]);
-
-  useEffect(() => {
-    if (instrument.length !== sampleSize) setSampleSize(instrument.length);
-  }, [instrument.length]);
-
-  const getCells = useMemo(() => {
-    console.log('rendering all cells');
-    let cells = [];
-    for (let i = 0; i < size; i++) {
-      const id = `cell-${i}`;
-      cells.push(
-        <div key={id} className={`cell ${id}`}>
-          <CellIcon />
-          <Overview cell={i} size={sampleSize} />
-        </div>
-      );
-    }
-    return cells;
-  }, [pattern.length, instrument.length]);
-
-  return <div id='grid'>{getCells}</div>;
+  const cells = getCells();
+  return <div id='grid'>{cells}</div>;
 };
 
-const Overview = ({ cell, size = 9 }) => {
-  console.log('rendering overview cell');
-  let overview = [];
+const Cell = ({ id }) => {
+  const soundCells = getSoundCells(id);
+  return (
+    <div className={`cell ${id}`}>
+      <CellIcon />
+      <div id='sound-cells'>{soundCells}</div>
+    </div>
+  );
+};
+
+const SoundCell = ({ id, classes }) => {
+  return (
+    <div id={id} className={classes}>
+      <CircleIcon />
+    </div>
+  );
+};
+
+const getCells = (size = 64) => {
+  let cells = [];
   for (let i = 0; i < size; i++) {
-    const id = `${cell}-${i}`;
-    overview.push(
-      <div key={id} id={id} className={`overview-sample color${i}`}>
-        <CircleIcon />
-      </div>
+    const id = `cell-${i}`;
+    cells.push(<Cell key={id} id={id} />);
+  }
+  return cells;
+};
+
+const getSoundCells = (id, size = 9) => {
+  let soundCells = [];
+  for (let i = 0; i < size; i++) {
+    const soundId = `${id}-${i}`;
+    const soundClasses = `sound-cell color${i}`;
+    soundCells.push(
+      <SoundCell key={soundId} id={soundId} classes={soundClasses} />
     );
   }
-  return <div id='overview'>{overview}</div>;
+  return soundCells;
 };
-
-// const grid64 = createGrid(64);
-// const grid9 = createGrid(9);
-
-// export const Grid = () => {
-//   const { pattern, setPattern } = useContext(Pattern);
-//   const { selectedSample, samples } = useContext(Samples);
-//   const [cells, setCells] = useState();
-
-//   const handleToggle = (newVol, i) => {
-//     setPattern((pattern) => {
-//       pattern[i][selectedSample] = newVol;
-//       return pattern;
-//     });
-//   };
-
-//   const Cell = ({ cell, i, handleToggle }) => {
-//     const [vol, setVol] = useState(pattern[i][selectedSample]);
-
-//     const toggleCell = () => {
-//       const newVol = vol === 0 ? 1 : vol === 1 ? 0.5 : 0;
-//       setVol(newVol);
-//       handleToggle(newVol, i);
-//     };
-
-//     let classes = 'cell';
-//     let current = cell[selectedSample] || null;
-//     if (current) classes += ` on color${samples[selectedSample].color}`;
-//     const iconStyle = { opacity: current ? vol : 1 };
-
-//     console.log('rendering cell: ', i);
-
-//     return (
-//       <div className={classes} onMouseDown={() => toggleCell(i)}>
-//         <CellIcon style={iconStyle} />
-//         <div className='overview'>
-//           <Overview cell={cell} i={i} />
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   const Overview = ({ cell, i }) =>
-//     Object.entries(samples).map(([sample, { color }], index) => {
-//       let sampleClass = 'overview-sample';
-//       let currentSample = cell[sample] || null;
-//       if (currentSample) sampleClass += ` color${color} full`;
-
-//       console.log('rendering overview: ', index);
-//       return (
-//         <div key={i + index + sample} className={sampleClass}>
-//           <CircleIcon />
-//         </div>
-//       );
-//     });
-
-//   const getCells = () => {
-//     const newCells = pattern.map((cell, i) => {
-//       const id = `cell-${i}`;
-//       return <Cell key={id} cell={cell} i={i} handleToggle={handleToggle} />;
-//     });
-//     setCells(newCells);
-//   };
-
-//   useEffect(() => {
-//     getCells();
-//   }, [selectedSample]);
-
-//   return <div id='grid'>{cells}</div>;
-// };
