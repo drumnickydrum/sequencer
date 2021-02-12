00c652ca82f52e43f311354e1fc860f5a4e84401
nfcarbone <nfcarbone@gmail.com>
2021-02-12 15:26:15 -0500

    SoundSelector memoized as well

--- src/Sections/SoundSelector.js
+++ src/Sections/SoundSelector.js
@@ -1,22 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { Instrument } from '../Providers/Instrument';
 import { Editor } from '../Providers/Editor';
 
 export const SoundSelector = () => {
   const { instrument } = useContext(Instrument);
-  const { setSelectedSound } = useContext(Editor);
+  const { selectedSound, setSelectedSound } = useContext(Editor);
 
   return (
     <div id='sound-selector'>
       {instrument.map((sound, i) => (
-        <div
+        <SoundBtn
           key={`sound-selector-${sound.name}`}
-          className={`sound border${sound.color}`}
-          onClick={() => setSelectedSound(i)}
-        >
-          {sound.name}
-        </div>
+          i={i}
+          sound={sound}
+          selectedSound={selectedSound}
+          setSelectedSound={setSelectedSound}
+        />
       ))}
     </div>
   );
+};
+
+const SoundBtn = ({ i, sound, selectedSound, setSelectedSound }) => {
+  const soundBtnMemo = useMemo(() => {
+    let classes = 'sound';
+    if (i === selectedSound) classes += ` border${sound.color}`;
+    return (
+      <div className={classes} onClick={() => setSelectedSound(i)}>
+        {sound.name}
+      </div>
+    );
+  });
+  return soundBtnMemo;
 };
